edf866f45e10d3b9d0b0fb15ee9e95b3d3641d73
Lisa Leung <110@holbertonschool.com>
2018-02-21 15:02:43 +0000

    removed method

--- level_0/hodor_0.py
+++ level_0/hodor_0.py
@@ -2,13 +2,11 @@
 
 import requests
 
-def hodor_1024(id):
-    URL = "http://158.69.76.135/level0.php"
-    payload = {
-        'id': id,
-        'holdthedoor': 'Submit'
-    }
-    for i in range(1024):
-        r = requests.post(URL, data=payload)
-
-hodor_1024("110")
+ID = 110
+URL = "http://158.69.76.135/level0.php"
+payload = {
+    'id': ID,
+    'holdthedoor': 'Submit'
+}
+for i in range(1024):
+    r = requests.post(URL, data=payload)
